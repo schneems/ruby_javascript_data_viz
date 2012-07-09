7bac9123d436270a19065dedb785095d036d64b5
schneems <richard.schneeman@gmail.com>
2012-07-08 23:29:12 -0500

    initial exercise

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,26 +1,25 @@
 class ProductsController < ApplicationController
-	
+
 	def index
 		@products = Product.includes(:user).all
-		
 	end
 
 	def create
-  		@product = Product.new(params[:product])
+		@product = Product.new(params[:product])
 
-  		respond_to do |format|
-    		if @product.save
-     		 format.html { render :action => "create" }
-     		 format.json { render :json => @product }
-   			else
-      		 format.html { render :action => "new" }
-    		 format.json { render :json => @product.errors, :status => :unprocessable_entity }
-   			end
+		respond_to do |format|
+  		if @product.save
+   		 format.html { render :action => "create" }
+   		 format.json { render :json => @product }
+ 			else
+    		 format.html { render :action => "new" }
+  		 format.json { render :json => @product.errors, :status => :unprocessable_entity }
  			end
+		end  
 	end
 
 	def new
-  		@product = Product.new
+		@product = Product.new
 	end
 
 	def show
@@ -32,27 +31,26 @@ class ProductsController < ApplicationController
 	end
 
 	def update
-    	@product = Product.where(:id => params[:id]).first
+  	@product = Product.where(:id => params[:id]).first
 
-   		respond_to do |format|
-      		if @product.update_attributes(params[:product])
-        		format.html { redirect_to @product, :notice => 'Product was successfully updated.' }
-        		format.json { head :no_content }
-      		else
-        	format.html { render action: "edit" }
-        	format.json { render json: @product.errors, :status => :unprocessable_entity }
-    		end
-    		end
+ 		respond_to do |format|
+      if @product.update_attributes(params[:product])
+    		format.html { redirect_to :back, :notice => 'Product was successfully updated.' }
+    		format.json { render json: @product }
+      else
+      	format.html { render action: "edit" }
+      	format.json { render json: @product.errors, :status => :unprocessable_entity }
+      end
   	end
+	end
 
 	def destroy
  		@product = Product.where(:id => params[:id]).first
-  		@product.destroy
+		@product.destroy
 
-  		respond_to do |format|
-    		format.html { redirect_to products_url }
-    		format.json { head :no_content }
-  		end
+		respond_to do |format|
+  		format.html { redirect_to products_url }
+  		format.json { head :no_content }
+		end
 	end
-
 end
